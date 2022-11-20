ea6ce71acd1c038ff86de6678b07028da695e142
Valetina-2608 <valusha2608@gmail.com>
2022-11-20 20:22:49 +0200

    Add function openWindowSearch

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 /* Script */
+
+
+/* Show and hide search window */
+
+let search_btn= document.getElementById('search_btn');
+let modal = document.querySelector('.modal');
+let content = document.querySelector('.content');
+search_btn.addEventListener('click', openWindowSearch);
+content.addEventListener('click', closeModal);
+	
+function openWindowSearch(){
+     modal.style.display = "block"; 
+}
+
+
+
+function closeModal(){
+    modal.style.display = "none"; 
+}
