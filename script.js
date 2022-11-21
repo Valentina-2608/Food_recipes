c5a7e76d311c754a09ce577b6be86b37ac1a4a9e
Valetina-2608 <valusha2608@gmail.com>
2022-11-21 18:52:43 +0200

    Find and correct mistake

--- script.js
+++ script.js
@@ -5,9 +5,10 @@
 
 let search_btn= document.getElementById('search_btn');
 let modal = document.querySelector('.modal');
+let close_modal = document.querySelector('.close_modal');
 let content = document.querySelector('.content');
 search_btn.addEventListener('click', openWindowSearch);
-content.addEventListener('click', closeModal);
+close_modal.addEventListener('click', closeModal);
 	
 function openWindowSearch(){
      modal.style.display = "block"; 
