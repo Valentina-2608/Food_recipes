9655a3918c5821fe0446213124ccbdb7e09b3271
Valetina-2608 <valusha2608@gmail.com>
2022-11-21 19:07:40 +0200

    Add event to input_find

--- script.js
+++ script.js
@@ -4,10 +4,12 @@
 /* Show and hide search window */
 
 let search_btn= document.getElementById('search_btn');
+let input_find = document.getElementById('input_find');
 let modal = document.querySelector('.modal');
 let close_modal = document.querySelector('.close_modal');
 let content = document.querySelector('.content');
 search_btn.addEventListener('click', openWindowSearch);
+input_find.addEventListener('click', openWindowSearch);
 close_modal.addEventListener('click', closeModal);
 	
 function openWindowSearch(){
