e921967504385b2979503f68032d79be1fc0caa2
Valetina-2608 <valusha2608@gmail.com>
2022-11-27 17:00:15 +0200

    Add script for page search

--- script.js
+++ script.js
@@ -3,11 +3,10 @@
 
 /* Show and hide search window */
 
-let search_btn= document.getElementById('search_btn');
+let search_btn = document.getElementById('search_btn');
 let input_find = document.getElementById('input_find');
 let modal = document.querySelector('.modal');
 let close_modal = document.querySelector('.close_modal');
-let content = document.querySelector('.content');
 search_btn.addEventListener('click', openWindowSearch);
 input_find.addEventListener('click', openWindowSearch);
 close_modal.addEventListener('click', closeModal);
@@ -21,3 +20,6 @@ function openWindowSearch(){
 function closeModal(){
     modal.style.display = "none"; 
 }
+
+
+
