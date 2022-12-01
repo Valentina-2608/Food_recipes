05b2d2ba6a23b4ad878a11ff098a3fea0e503be6
Valetina-2608 <valusha2608@gmail.com>
2022-12-01 20:23:39 +0200

    Add recipes.js

---
+++ recipes.js
@@ -0,0 +1,45 @@
+let recipes = `[
+    { 
+        "id":1,
+        "image":"img/black_cake.jpg",
+        "caption": "Black cake",
+        "ingredients": "prune, current, cherriesm raisins, citrus peel, cherry brandy, sugar, flower, eggs, lemon"
+    },
+
+    { 
+        "id":2,
+        "image":"img/pumkin_cake.jpg",
+        "caption": "Pumpkin Spice Crumb Cake",
+        "ingredients": "flour, larg eggs, cinnaman, pumpkin purée, sugar, vegetable oil, vanilla extract, grated fresh ginger"
+    },
+    { 
+        "id":3,
+        "image":"img/blueberry_cake.jpg",
+        "caption": "Blueberry Muffin Cake",
+        "ingredients": "flour, sugar, butter, milk, blueberries, vanilla extract, egg, ground cinnamon"
+    },
+    { 
+        "id":4,
+        "image":"img/sticky_pudding.jpg",
+        "caption": "Sticky toffee apple pudding",
+        "ingredients": "butter, flour, sugar, baking powder, milk, egg, bramley apples, vanille extract"
+    },
+    { 
+        "id":5,
+        "image":"img/apple_walnut_cake.jpg",
+        "caption": "Apple-Walnut Upside-Down Cake",
+        "ingredients": "walnuts, flour, baking soda,baking powder, sugar, cinnamon, eggs, butter, baking apples, Greek yogurt"
+    },
+    { 
+        "id":6,
+        "image":"img/lemon_cake.jpg",
+        "caption": "Lemon Cake with Fruit",
+        "ingredients": "butter, flour, baking powder, lemon, eggs, pear or apple brandy, pear or apples, blueberries"
+    },
+    { 
+        "id":6,
+        "image":"img/honey_cake.jpg",
+        "caption": "Honey cake",
+        "ingredients": "flour, baking powder, butter, honey, eggs, milk, orange zest, almond flakes, lemon juice, water"
+    }
+    ]`
