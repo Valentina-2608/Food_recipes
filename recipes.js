c839c780482330ad15055913a91c27fbf03a2e5e
Valetina-2608 <valusha2608@gmail.com>
2022-12-02 19:54:07 +0200

    Change file

--- recipes.js
+++ recipes.js
@@ -41,5 +41,78 @@ let recipes = `[
         "image":"img/honey_cake.jpg",
         "caption": "Honey cake",
         "ingredients": "flour, baking powder, butter, honey, eggs, milk, orange zest, almond flakes, lemon juice, water"
-    }
+    },
+    { 
+        "id":7,
+        "image":"img/burgers-caprese.jpg",
+        "caption": "Caprese Burger",
+        "ingredients": "balsamic vinegar, virgine olive oil, salt, pepprer, slice tomato, tomato paste, Parmesan cheese, Mozzarella cheese, hamburger buns"
+    },
+    { 
+        "id":8,
+        "image":"img/mushroom-burger.jpeg",
+        "caption": "Mushroom Veggie Burger",
+        "ingredients":"olive oil, mushrooms, black pepper, oregano, breadcrumbs, eggs, shredded Parmigiano-Reggiano cheese"
+    },
+    { 
+        "id":8,
+        "image":"img/chicken-burger.jpg",
+        "caption": "Baked Chicken Burger",
+        "ingredients":"ground chicken, egg, vegetable oil, liquid smoke flavoring, garlic powder, fresh basil"
+    },
+    { 
+        "id":9,
+        "image":"img/caesar_salad.jpg",
+        "caption": "Caesar Salad Supreme",
+        "ingredients":"garlic, mayonnaise, Parmesan cheese, lemon juice, olive oil, day-old bread, anchovy fillets"
+    },
+    { 
+        "id":9,
+        "image":"img/fruit_salad.jpg",
+        "caption": "Summer Fruit Salad",
+        "ingredients": "pineapple, kiwi, bananas, oranges, grapes, blueberries, strawberried, fresh lemon and orange juice"
+    },
+    { 
+        "id":9,
+        "image":"img/vegetable_salad.jpeg",
+        "caption": "Vegetable Salad",
+        "ingredients": "lettuce, radishes, tomatoes, green onion, cucumber, red bell pepper, olive oil, lemon juice, basil"
+    },
+    { 
+        "id":10,
+        "image":"img/chicken_salad.jpg",
+        "caption": "Chicken Salad",
+        "ingredients": "chicken, mayonnaise, celery, almonds, lemon juice, black pepper"
+    },
+    { 
+        "id":11,
+        "image":"img/greek_salad.jpg",
+        "caption": "Greek Orzo Salad",
+        "ingredients": "orzo pasta, cucumber, tomato, red onion, feta cheese, lemon juice, oregano, black olives, artichokes"
+    },
+    { 
+        "id":11,
+        "image":"img/mozzarella_salad.jpg",
+        "caption": "Tomato Mozzarella Salad",
+        "ingredients": "tomatoes, mozzarella cheese, olive oil, balsamic vinegar, black pepper, fresh basil"
+    },
+     { 
+        "id":12,
+        "image":"img/green_pea_salad.jpg",
+        "caption": "Green Pea Salad With Cheddar Cheese",
+        "ingredients": "green sweets peas, eggs, Cheddar cheese, mayonnaise, onion, black pepper"
+     },
+     { 
+        "id":13,
+        "image":"img/pasta_salad.jpg",
+        "caption": "Simple Pasta Salad",
+        "ingredients": "rotini pasta, cucumbers, tomatoes, green onion, Parmesan cheese, Italian seasoning"
+     },
+     { 
+        "id":13,
+        "image":"img/s_s_salad.jpg",
+        "caption": "Spinach and Strawberry Salad",
+        "ingredients": "spinach, strawberries, sugar, white wine vinegar, sesame seeds, poppy seeds"
+     }
+
     ]`
