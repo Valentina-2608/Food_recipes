273c7dff0e7333b53b4294e71fca5a6046da3744
Valetina-2608 <valusha2608@gmail.com>
2022-12-04 19:33:14 +0200

    Change file

--- recipes.js
+++ recipes.js
@@ -37,226 +37,354 @@ let recipes = `[
         "ingredients": "butter, flour, baking powder, lemon, eggs, pear or apple brandy, pear or apples, blueberries"
     },
     { 
-        "id":6,
+        "id":7,
         "image":"img/honey_cake.jpg",
         "caption": "Honey Cake",
         "ingredients": "flour, baking powder, butter, honey, eggs, milk, orange zest, almond flakes, lemon juice, water"
     },
     { 
-        "id":7,
+        "id":8,
         "image":"img/burgers-caprese.jpg",
         "caption": "Caprese Burger",
         "ingredients": "balsamic vinegar, virgine olive oil, salt, pepprer, slice tomato, tomato paste, Parmesan cheese, Mozzarella cheese, hamburger buns"
     },
     { 
-        "id":8,
+        "id":9,
         "image":"img/mushroom-burger.jpeg",
         "caption": "Mushroom Veggie Burger",
         "ingredients":"olive oil, mushrooms, black pepper, oregano, breadcrumbs, eggs, shredded Parmigiano-Reggiano cheese"
     },
     { 
-        "id":8,
+        "id":10,
         "image":"img/chicken-burger.jpg",
         "caption": "Baked Chicken Burger",
         "ingredients":"ground chicken, egg, vegetable oil, liquid smoke flavoring, garlic powder, fresh basil"
     },
     { 
-        "id":9,
+        "id":11,
         "image":"img/caesar_salad.jpg",
         "caption": "Caesar Salad Supreme",
         "ingredients":"garlic, mayonnaise, Parmesan cheese, lemon juice, olive oil, day-old bread, anchovy fillets"
     },
     { 
-        "id":9,
+        "id":12,
         "image":"img/fruit_salad.jpg",
         "caption": "Summer Fruit Salad",
         "ingredients": "pineapple, kiwi, bananas, oranges, grapes, blueberries, strawberried, fresh lemon and orange juice"
     },
     { 
-        "id":9,
+        "id":13,
         "image":"img/vegetable_salad.jpeg",
         "caption": "Vegetable Salad",
         "ingredients": "lettuce, radishes, tomatoes, green onion, cucumber, red bell pepper, olive oil, lemon juice, basil"
     },
     { 
-        "id":10,
+        "id":14,
         "image":"img/chicken_salad.jpg",
         "caption": "Chicken Salad",
         "ingredients": "chicken, mayonnaise, celery, almonds, lemon juice, black pepper"
     },
     { 
-        "id":11,
+        "id":15,
         "image":"img/greek_salad.jpg",
         "caption": "Greek Orzo Salad",
         "ingredients": "orzo pasta, cucumber, tomato, red onion, feta cheese, lemon juice, oregano, black olives, artichokes"
     },
     { 
-        "id":11,
+        "id":16,
         "image":"img/mozzarella_salad.jpg",
         "caption": "Tomato Mozzarella Salad",
         "ingredients": "tomatoes, mozzarella cheese, olive oil, balsamic vinegar, black pepper, fresh basil"
     },
      { 
-        "id":12,
+        "id":17,
         "image":"img/green_pea_salad.jpg",
         "caption": "Green Pea Salad With Cheddar Cheese",
         "ingredients": "green sweets peas, eggs, Cheddar cheese, mayonnaise, onion, black pepper"
      },
      { 
-        "id":13,
+        "id":18,
         "image":"img/pasta_salad.jpg",
         "caption": "Simple Pasta Salad",
         "ingredients": "rotini pasta, cucumbers, tomatoes, green onion, Parmesan cheese, Italian seasoning"
      },
      { 
-        "id":13,
+        "id":19,
         "image":"img/s_s_salad.jpg",
         "caption": "Spinach and Strawberry Salad",
         "ingredients": "spinach, strawberries, sugar, white wine vinegar, sesame seeds, poppy seeds"
      },
      { 
-        "id":14,
+        "id":20,
         "image":"img/roast_chicken.jpg",
         "caption": "Roast Chicken",
         "ingredients": "chicken, salt, pepper, onion powder, celery"
      },
      { 
-        "id":15,
+        "id":21,
         "image":"img/smoked_chicken_drumsticks.jpg",
         "caption": "Smoked Chicken Drumsticks",
         "ingredients": "chicken drumsticks, vegetable oil"
      },
      { 
-        "id":16,
+        "id":22,
         "image":"img/marmalade_chicken.jpg",
         "caption": "Orange Marmalade Chicken",
         "ingredients": "chicken drumsticks, salt, pepper, orange marmalade, barbeque sauce, soy sauce"
      },
      { 
-        "id":17,
+        "id":23,
         "image":"img/apricot_chicken.jpeg",
         "caption": "Apricot Chicken Curry",
         "ingredients": "chicken drumsticks, salt, pepper, olive oil, garlic, apricot nectar, dried apricots, onion, carrots, water chestnuts"
      },
      { 
-        "id":17,
+        "id":24,
         "image":"img/lemon_chicken.jpg",
         "caption": "Italian Chicken with Garlic and Lemon",
         "ingredients": "chicken drumsticks, potatoes, wine vinegar, lemons, garlic, oregano, onion, parsley, salt, pepper"
      },
      { 
-        "id":18,
+        "id":25,
         "image":"img/coconut_chicken.jpg",
         "caption": "Green Coconut Chicken",
         "ingredients": "Chicken legs, salt, vegetable oil, coconut milk, garlic, pepper, cilantro, onion, ginger, lemon juice, sweet potato"
     },
     { 
-        "id":19,
+        "id":26,
         "image":"img/grapefruit_chicken.jpeg",
         "caption": "Roast Chicken with Grapefruit",
         "ingredients": "red grapefruit, chicken, pepper, parsley, bacon, olive oil, cognac or brandy, "
     },
     { 
-        "id":20,
+        "id":27,
         "image":"img/roast_turkey.jpeg",
         "caption": "Roast turkey",
         "ingredients": "Turkey, salt, pepper, turkey stock"
     },
     { 
-        "id":21,
+        "id":28,
         "image":"img/grill_turkey.jpg",
         "caption": "Gas Grill Turkey",
         "ingredients": "Turkey, water, butter, salt, chicken bouillon granules, black pepper"
     },
     { 
-        "id":22,
+        "id":29,
         "image":"img/turkey_and_apples.jpg",
         "caption": "Apple-Stuffed Turkey",
         "ingredients": "Turkey, vegetable oil, apples, unpeeled apples"
     },
     { 
-        "id":23,
+        "id":30,
         "image":"img/honey_turkey.jpg",
         "caption": "Honey-Smoked Turkey",
         "ingredients": "Turkey, honey, vegetable oil, pepper, salt, fresh basil, salt"
     },
     { 
-        "id":24,
+        "id":31,
         "image":"img/tuna_steak.jpg",
         "caption": "Marinated Tuna Steak",
         "ingredients": "orange juice, soy sauce, olive oil, parsley, lemon juice, garlic, fresh oregano, tuna steaks"
         
     },
     { 
-        "id":25,
+        "id":32,
         "image":"img/grilled_salmon.jpg",
         "caption": "Grilled Salmon",
         "ingredients": "salmon, black pepper, garlic powder, soy sauce, brown sugar, water, vegetable oil "
     },
     { 
-        "id":26,
+        "id":33,
         "image":"img/maple_salmon.jpg",
         "caption": "Maple Salmon",
         "ingredients": "maple syrup, soy sauce, garlic, seasonings, salmon "
     },
     { 
-        "id":26,
+        "id":34,
         "image":"img/cedar_salmon.jpeg",
         "caption": "Cedar Planked Salmon",
         "ingredients": "soy sauce, vegetable oil, rice vinegar, sesame oil, green onions, garlic, salmon fillets"
     },
     { 
-        "id":27,
+        "id":35,
         "image":"img/haddock.jpg",
         "caption": "Simple Broiled Haddock",
         "ingredients": " haddock fillets, onion powder, paprika, garlic powder, black pepper, salt, butter, lemon"
     },
     { 
-        "id":28,
+        "id":36,
         "image":"img/flounder.jpg",
         "caption": "Baked Flounder with Panko and Parmesan",
         "ingredients": "flounder fillets, butter, bread crumbs, Parmesan cheese, salt, ground black pepper"
     },
     { 
-        "id":29,
+        "id":37,
         "image":"img/cheese_soup.jpg",
         "caption": "Creamy Slow Cooker Potato Cheese Soup",
         "ingredients": "butter, onion, flour, water, carrots, celery, chicken soup base, potatoes ,Cheddar cheese, cooked bacon"
     },
     { 
-        "id":30,
+        "id":38,
         "image":"img/tomato_soup.jpg",
         "caption": "Fresh Tomato Soup",
         "ingredients": "tomatoes, onion, clove, garlic, butter, chicken broth, salt, flour"
     },
     { 
-        "id":31,
+        "id":39,
         "image":"img/ham_soup.jpg",
         "caption": "Delicious Ham and Potato Soup",
         "ingredients": "potatoes, ham, celery, onion, chicken bouillon granules, pepper, butter, flour, milk"
     },
     { 
-        "id":32,
+        "id":40,
         "image":"img/turkey_soup.jpg",
         "caption": "Hearty Barley Turkey Soup",
         "ingredients": "turkey. onion, carrot, celery, peppercorns, dried thyme, water, barley"
     },
     { 
-        "id":33,
+        "id":41,
         "image":"img/vegetable_soup.jpg",
         "caption": "Vegetable Soup",
         "ingredients": "tomatoes, chicken broth, tomato-vegetable juice, carrots, potatoe, green beans, fresh corn kernels "
     },
     { 
-        "id":34,
+        "id":42,
         "image":"img/broccoli_soup.jpg",
         "caption": "Copycat Panera Broccoli Cheddar Soup",
         "ingredients": "butter, onion, flour, milk, chicken stock, broccoli florets, carrots, Cheddar cheese, pepper"
     },
     { 
-        "id":35,
+        "id":43,
         "image":"img/noodle_soup.jpg",
         "caption": "Incredibly Easy Chicken and Noodles",
         "ingredients": "chicken broth, chicken breast meat, onion powder, garlic powder,  frozen egg noodles"
+    },
+    { 
+        "id":44,
+        "image":"img/roasted_vegetables.jpg",
+        "caption": "Roasted vegetables",
+        "ingredients": "cauliflower, broccoli, sweet potatoe, red and yellow peppers, red onion"
+    },
+    { 
+        "id":45,
+        "image":"img/fry_vegetables.jpg",
+        "caption": "Easy Stir Fry Vegetables",
+        "ingredients": "broccoli, bell pepper, onion, carrots, mushrooms, garlic, olive oil"
+    },
+    { 
+        "id":46,
+        "image":"img/carrots.jpg",
+        "caption": "Perfect Sauteed Carrots",
+        "ingredients": "carrots, olive oil, fresh herbs, salt"
+    },
+    { 
+        "id":47,
+        "image":"img/parmesan_potatoe.jpg",
+        "caption": "Parmesan Potato Wedges",
+        "ingredients": "potatoes, butter, garlis powder, basil, Shredded Parmesan cheese"
+    },
+    { 
+        "id":48,
+        "image":"img/tomato_fritters.jpg",
+        "caption": "Tomato fritters",
+        "ingredients": "cherry tomatoes, oregano, olive oil, wine vinegar, red onion, flour, egg, feta cheese"
+    },
+    { 
+        "id":49,
+        "image":"img/courgettes.jpg",
+        "caption": "Pea & ricotta stuffed courgettes",
+        "ingredients": "mint, peas, ricotta cheese, cheddar cheese, lemon, cherry tomatoes, onions, basmati rice, olive oil"
+    },
+    { 
+        "id":50,
+        "image":"img/beetroot_curry.jpg",
+        "caption": "Beetroot curry",
+        "ingredients": "beets, cherry tomatoes, wild rice, lemon, coconut milk, onions, garlic, mustard seeds"
+    },
+    { 
+        "id":51,
+        "image":"img/panzarella.jpg",
+        "caption": "Roasted veg panzanella",
+        "ingredients": " anchovy fillets, garlic, olive oil, wine vinegar, tomatoes, capers, ciabatta, red peppers, artichokes"
+    },
+    { 
+        "id":52,
+        "image":"img/chocolate_bread.jpg",
+        "caption": "Chocolate-chip soda bread",
+        "ingredients": "hazelnuts, egg, milk, plain flour, soda, buttermilk, oats"
+    },
+    { 
+        "id":53,
+        "image":"img/banana_bread.jpg",
+        "caption": "Banana Banana Bread",
+        "ingredients": "flour, soda, salt, butter, brown sugar, eggs, bananas"
+    },
+    { 
+        "id":54,
+        "image":"img/monkey_bread.jpg",
+        "caption": "Best Monkey Bread",
+        "ingredients": "biscuit dough, sugar, cinnamon, walnuts, margarine and brown sugar, raisins"
+    },
+    { 
+        "id":55,
+        "image":"img/zucchini_bread.jpg",
+        "caption": "Zucchini Bread",
+        "ingredients": "flour, baking powder, cinnamon, eggs, zucchini, walnuts, vanilla extract, sugar"
+    },
+    { 
+        "id":56,
+        "image":"img/chocolate_banana_bread.jpg",
+        "caption": "Banana Chocolate Chip Bread",
+        "ingredients": "flour, leavener, salt, bananas, milk, cinnamon, sugar, eggs, chocolate chips"
+    },
+    { 
+        "id":57,
+        "image":"img/mango_salsa.jpg",
+        "caption": "Fresh Mango Salsa",
+        "ingredients": "roma tomatoes,mango, onion, sugar, cilantro, lime juice, cider vinegar, salt, pepper"
+    },
+    { 
+        "id":58,
+        "image":"img/watermelon_bowl.jpg",
+        "caption": "Watermelon Fruit Bowl",
+        "ingredients": "watermelon, melon, oranges, pineapple chunks, grapes, strawberries, sugar"
+    },
+    { 
+        "id":59,
+        "image":"img/bruschetta.jpg",
+        "caption": "Strawberry Bruschetta",
+        "ingredients": "French baguette, strawberries, sugar, butter"
+    },
+    { 
+        "id":60,
+        "image":"img/peach_salsa.jpg",
+        "caption": "Easy Peach Salsa",
+        "ingredients": "olive oil, onion, tomatoes, peaches, honey, lemon juice"
+    },
+    { 
+        "id":61,
+        "image":"img/roast_oranges.jpg",
+        "caption": "Roasted Oranges with Thyme",
+        "ingredients": "oranges, honey, olive oil, thyme, Sriracha sauce"
+    },
+    { 
+        "id":62,
+        "image":"img/crumble.jpg",
+        "caption": "Roasted stone fruit crumble",
+        "ingredients": "peaches, plums, cherries, strawberries, honey, orange, butter,  rolled oats, almond"
+    },
+    { 
+        "id":63,
+        "image":"img/apple_crumble.jpg",
+        "caption": "Classic apple crumble",
+        "ingredients": "apples,sugar, butter, lemon, flour"
+    },
+    { 
+        "id":64,
+        "image":"img/sponges.jpg",
+        "caption": "Strawberry and cream sandwich sponge",
+        "ingredients": "butter, sugar, vanilla extract, eggs,flour, cream, milk, baking powder, strawberries"
     }
+   
+
     ]`
